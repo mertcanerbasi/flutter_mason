13af3f01f7e0ee4cd9bcafbbb535e223a206e288
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-09-05 16:06:34 +0300

    environment

--- bricks/core/__brick__/lib/main_production.dart
+++
@@ -1,7 +0,0 @@
-import 'bootstrap.dart';
-import './feature/page/app/app.dart';
-import 'package:injectable/injectable.dart';
-
-void main() async {
-  bootstrap(() => const App(), Environment.prod);
-}
